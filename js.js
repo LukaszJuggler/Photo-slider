600cb8c08ddd1732da8c3b166bf57f41bba2ba25
lukasz <lukaszu@hotmail.com>
2018-12-05 18:08:47 +0100

    adding a loader

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
+//DOM elements
 const slider = document.querySelector('#slider');
 const fullscreen = document.querySelector('#fullscreen');
+const loader = document.querySelector('#loader');
 const photo1 = document.querySelector('#photo1');
 const photo2 = document.querySelector('#photo2');
 const photo3 = document.querySelector('#photo3');
@@ -7,20 +9,35 @@ const photo4 = document.querySelector('#photo4');
 const photo5 = document.querySelector('#photo5');
 const photos = [photo1, photo2, photo3, photo4, photo5];
 
-let isFullscreen = false;
-let sliderLeft = 0;
-let animate = true;
 const photoWidth = 540;
+let isFullscreen = false;
+let sliderMargin = 0;
+let animate = true;
 
-//set flex order for each photo
-photos.forEach((photo, i) => {
-      photo.style.order = i+1;
+
+window.addEventListener('load', () => {
+      //set flex order for each photo
+      photos.forEach((photo, i) => {
+            photo.style.order = i + 1;
+      });
+
+      // remove loader and show photos
+      loader.parentNode.removeChild(loader);
+      slider.classList.add('visible');
+
+      // enable photo manipulation
+      slider.addEventListener("mouseover", () => animate = false);
+      slider.addEventListener("mouseleave", () => animate = true);
+      slider.addEventListener("click", (e) => fullscreen__on(e.target));
+      fullscreen.addEventListener("click", fullscreen__off);
+      
+      //start animating
+      repeat();
 });
-
-
-let isPhotoOffScreen = function() {
-      return -sliderLeft >= photoWidth;
+let isPhotoOffScreen = () => {
+      return -sliderMargin >= photoWidth;
 }
+
 function changeImgOrder() {
       photos.forEach(photo => {
             photo.style.order--;
@@ -28,45 +45,29 @@ function changeImgOrder() {
                   photo.style.order = photos.length;
             }
       });
-}
-
-function repeat() {
-    if (!isFullscreen && animate) {
-            if (isPhotoOffScreen()) {
-                  sliderLeft = 0;
-                  changeImgOrder();
-            }
-            //move slider
-            sliderLeft-=2;
-            slider.style.marginLeft = `${sliderLeft}px`;
-    }
-    requestAnimationFrame(repeat);
-}
+};
 
 function fullscreen__on(clickedPhoto) {
       fullscreen.innerHTML = `<img src=${clickedPhoto.src} alt="random photo">`;
       isFullscreen = true;
       fullscreen.classList.add("show__photo");
-}
-function fullscreen__off(clickedPhoto) {
-      
+};
+
+function fullscreen__off() {   
       fullscreen.innerHTML = ``;
       isFullscreen = false;
       fullscreen.classList.remove("show__photo");
-}
-function startAnimation() {
-      animate = true;
-}
+};
 
-function stopAnimation() {
-      animate = false;
-}
-
-slider.addEventListener("mouseover", stopAnimation);
-slider.addEventListener("mouseleave", startAnimation);
-slider.addEventListener("click", function(e) {  
-      fullscreen__on(e.target);
-});
-fullscreen.addEventListener("click", fullscreen__off);
-
-repeat();
+function repeat() {
+      if (!isFullscreen && animate) {
+            if (isPhotoOffScreen()) {
+                  sliderMargin = 0;
+                  changeImgOrder();
+            }
+            //move slider
+            sliderMargin -= 2;
+            slider.style.marginLeft = `${sliderMargin}px`;
+      }
+      requestAnimationFrame(repeat);
+};
